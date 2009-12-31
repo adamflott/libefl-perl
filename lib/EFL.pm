51649d42140c9f63f923fce1f9c9a0a3fd6eb66a
Adam Flott <adam@npjh.com>
2009-12-31 16:00:46 -0500

    Initial commit of most functioning Perl EFL Evas and Elementary bindings

---
+++ lib/EFL.pm
@@ -0,0 +1,160 @@
+package EFL;
+
+use 5.10.0;
+
+use strict;
+use warnings;
+
+our $VERSION    = '0.50';
+our $XS_VERSION = $VERSION;
+$VERSION = eval $VERSION;  ## no critic
+
+use Carp;
+
+# TODO actually export
+
+1;
+
+__END__
+
+=head1 NAME
+
+EFL - Perl bindings for the Enlightenment Foundation Libraries
+
+
+=head1 SYNOPSIS
+
+Import all EFL functions:
+
+    use EFL qw(:all);
+
+Or selectively import only a subset:
+
+    use EFL qw(:elementary);
+
+Or multiple subsets:
+
+    use EFL qw(:evas :elementary);
+
+=head1 DESCRIPTION
+
+Perl bindings for the Enlightenment Foundation Libraries (EFL) Elementary
+library. The API was deliberatly kept as close to 1-to-1 mapping of the
+Elementary API as possible. Function parameters types and position are the same
+as you would in C (deviations are documented), Evas_Object pointers are just
+scalars, #defines are Perl constants, etc.
+
+For example, the C way:
+
+    elm_win_screen_position_get(Evas_Object *obj, int *x, int *y);
+
+This will be translated into the Perlish-C way as:
+
+    elm_win_screen_position_get($obj, \$x, \$y);
+
+As opposed to the Perl way:
+
+    my ($x, $y) = elm_win_screen_position_get($obj);
+
+This was intentional as the desired goal will to aid in porting to pure-C. But
+with the power of Perl and CPAN, it will be easier for rapid development and
+prototyping.
+
+
+=head1 REQUIRES
+
+=over
+
+=item * Linux (untested on non-Linux platforms)
+
+=item * Perl 5.10.0+ (I haven't tried 5.8.x yet)
+
+=item * gcc
+
+=item * C<pkg-config> can find the F<evas.pc> and F<elementary.pm> files (if not
+set C<PKG_CONFIG_PATH>)
+
+=item * evas and elmentary compiled successfully
+
+=item * linkable with -levas and -lelementary
+
+=back
+
+
+=head1 SUPPORTED
+
+Currently only the following are supported:
+
+=over
+
+=item * evas
+
+=item * elementary
+
+=back
+
+As I need more, I'll add them. Probably ecore, eina (may or may not
+apply), eet.
+
+
+=head1 EXPORT
+
+Nothing by default. You can either import the entire API with :all, or only a
+subset with the tag name being the EFL group, e.g. C<:elementary>.
+
+Check the EFL API documentation for how to correctly call functions.
+
+To see what functions can be export run C<misc/supported.pl -api <efl-subset>> and
+C<misc/supported.pl -tags <efl-subset>> for the Exporter tags available.
+
+
+=head1 SEE ALSO
+
+Main Enlightenment page: L<http://www.enlightenment.org>
+
+Elementary API: L<http://docs.enlightenment.org/auto/elementary/>
+
+Git Web Repository: L<http://git.npjh.com/?p=libefl-perl.git;a=summary>
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc EFL
+
+You can also look for information at:
+
+=over
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/EFL>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/EFL>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=EFL>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/EFL>
+
+=back
+
+
+=head1 AUTHOR
+
+Adam Flott, E<lt>adam@npjh.comE<gt>
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Adam Flott
+
+LGPL. EFL will have the same license as EFL itself.
+
+=cut
